f2e757d301e0cfb90314185d6b6ccc3aaa5c14a2
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2025-09-14 09:30:03 +0900

    add validate

--- app/models/business_partner.rb
+++ app/models/business_partner.rb
@@ -1,7 +1,7 @@
 class BusinessPartner < ApplicationRecord
   validates :name, presence: true
   validates :phone_number, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :age, presence: true
+  validates :age, presence: true, numericality: { only_integer: true }
 
   has_many :business_partners
 end
