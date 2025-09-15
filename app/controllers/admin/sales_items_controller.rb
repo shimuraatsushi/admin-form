955f659c442dcb9aae4f25b7e9c5424716372df8
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2025-09-15 10:33:49 +0900

    add sales_item new page

--- app/controllers/admin/sales_items_controller.rb
+++ app/controllers/admin/sales_items_controller.rb
@@ -6,4 +6,21 @@ class Admin::SalesItemsController < ApplicationController
   def show
     @sales_item = SalesItem.find(params[:id])
   end
+
+  def new
+    @sales_item = SalesItem.new
+  end
+
+  def create
+    @sales_item = SalesItem.new(sales_item_params)
+    return unless @sales_item.valid?
+    @sales_item.save
+   
+  end
+  
+  private
+
+  def sales_item_params
+    params.expect(sales_item: [:name])
+  end
 end
